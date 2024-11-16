164a9c928f046a919ac32109211b5fa50d4f9883
fizza897 <fizzaazam6@gmail.com>
2024-11-17 01:18:35 +0500

    done

--- Headline/Headline.tsx
+++ Headline/Headline.tsx
@@ -10,7 +10,7 @@ const Headline = () => {
     <div className='container mx-auto p-4 bg-secondary border-4'>
       <div className='w-full h-12 flex flex-col sm:flex-row md:flex-row  justify-between items-center'>
         <div className=' flex gap-4 text-sm sm:text-base'>
-          <p className='text-black font-normal mt-1.5'>Phone Number: 956 742 455 678</p>
+          <p className='text-black font-normal mt-1.5'>Phone Number:fizza 455 678</p>
           <p className='text-primary font-normal mt-1.5'>|</p>
           <p className='text-black font-normal mt-1.5'>Phone Number: 956 742 455 678</p>
         </div>
