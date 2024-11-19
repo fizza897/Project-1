e996247a09e18922c415c4e40fc1024653f98cfe
fizza897 <fizzaazam6@gmail.com>
2024-11-19 14:02:55 +0500

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-import RecentPost from "../../components/RecentPost/RecentPost";
-import ReacentPost from "../../components/RecentPost/RecentPost"
+
 export default function Home() {
   return (
     <>
