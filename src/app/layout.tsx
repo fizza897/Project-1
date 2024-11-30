94a3605dc9ab928a64cfe46b8c9a724f926d1c72
fizza897 <fizzaazam6@gmail.com>
2024-12-01 01:12:38 +0500

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 
 import type { Metadata } from "next";
 import { Roboto } from 'next/font/google'
-import localFont from "next/font/local";
 import "./globals.css";
 import Header from "../../Header/Header";
 import Footer from "../../Footer/Footer";
